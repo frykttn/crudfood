f30ed3c2543569cbe0496ca4989c0aef1f3f7a6d
Frykttn <bernedoflor234@gmail.com>
2025-10-16 09:52:44 -0300

    agregamos una tipografia e icono al logo del navbar

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -8,12 +8,13 @@ const Menu = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className='titulo fs-3'><i className="bi bi-egg-fried fs-2 me-2"></i>Crud Food</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Administrador</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
             
           </Nav>
         </Navbar.Collapse>
